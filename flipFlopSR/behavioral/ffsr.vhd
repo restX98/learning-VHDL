2c840ccc7133e8e832ea471f3e40e1eda341c502
Enrico Restuccia <enrico.restuccia@skylabs.it>
2023-08-18 00:35:00 +0200

    Flip Flop SR Asincrono - Behavioral + Testbench

---
+++ flipFlopSR/behavioral/ffsr.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ffsr is
+    port (
+        s, r : in bit;
+        q: buffer bit := '0';
+		qn: buffer bit := '1'
+    );
+end ffsr;
+
+architecture Beh of ffsr is
+
+begin
+	qn <= NOT (s OR q);
+    q <= NOT (r OR qn);
+end Beh;
---
+++ flipFlopSR/tb_ffsr.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity TB_ffsr is
+end TB_ffsr;
+
+architecture Beh of TB_ffsr is
+    component ffsr is
+        port (
+            s, r: in bit;
+            q, qn: inout bit
+        );
+    end component;
+    signal s, r, q, qn : bit;
+begin
+    dut: ffsr port map(s, r, q, qn);
+														  
+	s <= '0' after 0 ns, '1' after 10 ns , '0' after 11 ns, '1' after 40 ns, '0' after 41 ns;
+    r <= '0' after 0 ns , '1' after 20 ns, '0' after 21 ns, '1' after 30 ns, '0' after 31 ns;
+end Beh;
